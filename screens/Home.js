fa80fdec087c36f384523ed9e960f2cdbc8ca94d
Myungho Lee <leedox@naver.com>
2023-11-03 17:15:33 +0900

    테이블 추가

--- screens/Home.js
+++ screens/Home.js
@@ -1,40 +1,45 @@
 import { Button, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import {
+  Cell,
+  Row,
+  Rows,
+  Table,
+  TableWrapper,
+} from "react-native-reanimated-table";
+
+const words = {
+  header: ["단어", "의미", "메모"],
+  rows: [
+    ["able", "할수있는", 1],
+    ["abroad", "해외로", 1],
+  ],
+};
 
 const Home = ({ navigation }) => {
   return (
-    <>
-      <View style={styles.container}>
-        <View style={styles.word}>
-          <Text>단어</Text>
-        </View>
-        <View style={styles.meaning}>
-          <Text>의미</Text>
-        </View>
-      </View>
-      <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.word}
-          onPress={() => {
-            navigation.navigate("Detail");
-          }}
-        >
-          <View>
-            <Text>able</Text>
-          </View>
-        </TouchableOpacity>
-        <View style={styles.meaning}>
-          <Text>할수있는</Text>
-        </View>
-      </View>
-      <View>
-        <Button
-          title="go detail"
-          onPress={() => navigation.navigate("Detail")}
-        />
-      </View>
-    </>
+    <View style={styles.container}>
+      <Table
+        borderStyle={{
+          borderWidth: 1,
+          borderColor: "#c8e1ff",
+        }}
+      >
+        <Row
+          data={words.header}
+          flexArr={[2, 3, 1]}
+          style={styles.head}
+          textStyle={styles.headText}
+        ></Row>
+        <Rows
+          data={words.rows}
+          flexArr={[2, 3, 1]}
+          style={styles.row}
+          textStyle={styles.text}
+        ></Rows>
+      </Table>
+    </View>
   );
 };
 
@@ -42,9 +47,35 @@ export default Home;
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flex: 1,
+    padding: 10,
+    justifyContent: "flex-start",
+    backgroundColor: "#fff",
   },
+
+  head: {
+    height: 30,
+    backgroundColor: "#f1f8ff",
+  },
+
+  headText: {
+    fontSize: 15,
+    textAlign: "center",
+  },
+
+  row: {
+    height: 30,
+  },
+
+  row1: {
+    flexDirection: "row",
+    height: 30,
+  },
+
+  text: {
+    padding: 4,
+  },
+
   word: {
     flex: 1,
     margin: 5,
